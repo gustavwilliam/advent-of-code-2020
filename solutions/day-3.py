0466187a3aafbd2519b4138018a5fd669e09cc31
Gustav Odinger <65498475+gustavwilliam@users.noreply.github.com>
2020-12-03 13:01:26 +0100

    Add solution for both stars for day three

---
+++ solutions/day-3.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+def is_tree(pattern: List[str], row: int, col: int) -> bool:
+    """Checks if the input pattern has a tree at the input coordinates."""
+    width = len(pattern[0])    
+    pat_col = col-col//(width-1)*(width-1)
+
+    if pattern[row][pat_col] == "#":
+        return True
+    return False
+    
+
+def trees_in_route(pattern: List[str], right: int, down: int) -> int:
+    """Counts the number of trees in the given path."""
+    trees = 0
+    
+    for i, row in enumerate(range(0, len(pattern), down)):
+        if is_tree(pattern, row, i*right):
+            trees += 1
+            
+    return trees
